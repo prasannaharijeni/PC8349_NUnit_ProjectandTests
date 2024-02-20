e88af998c37f9624dc4fa400fdf3da17bcada141
Prasanna Chinnu <53172995+prasannaharijeni@users.noreply.github.com>
2024-02-19 21:29:13 -0500

    Add files via upload

---
+++ Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        //Prasanna Chinnu 8868349 Mutant test.
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                //Made some changes in the result
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+
+            
+            return result;
+        }
+    }
+}
